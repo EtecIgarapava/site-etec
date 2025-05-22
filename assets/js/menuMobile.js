0b2b7f9be85bcf920ad10d505b63b6dbd317cacd
RuanParreira <ruan.parreira@edu.uniube.br>
2025-05-22 14:55:57 -0300

    Atualização Header

--- assets/js/menuMobile.js
+++ assets/js/menuMobile.js
@@ -9,7 +9,7 @@ window.addEventListener("scroll", () => {
 });
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "100%";
 }
 
 function closeNav() {
