4fb0fd994e15c96b7165c8ade0c1e40ff30c8c18
RuanParreira <ruan.parreira@edu.uniube.br>
2025-05-21 21:54:32 -0300

    Projetos

--- assets/js/carrosel.js
+++ assets/js/carrosel.js
@@ -1,28 +1,39 @@
-const swiper = new Swiper('.swiper', {
-    // Geral
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 20,
-    autoplay: {
-        delay: 5000,
-    },
+const swiper = new Swiper(".swiper", {
+  // Geral
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 5000,
+  },
 
-    effect: 'coverflow',
-    coverflowEffect: {
-        rotate: 0,
-        slideShadows: true,
-    },
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 0,
+    slideShadows: true,
+  },
 
-    // Paginação
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-    },
+  // Paginação
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
 
-    // Botões de navegação
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+  // Botões de navegação
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
+    1023: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+  },
 });
